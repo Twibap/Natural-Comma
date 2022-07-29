9f76320acbaeddcbce39ad23ea75b462d90223aa
유태경 <dev.tkyoo@gmail.com>
2022-07-29 17:11:32 +0900

    Initial commit

---
+++ app/src/main/java/io/github/twibap/comma/MainActivity.kt
@@ -0,0 +1,18 @@
+package io.github.twibap.comma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.twibap.comma.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/io/github/twibap/comma/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.twibap.comma.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
